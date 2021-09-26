7f48ac923ab2b9aeee2baa4aa4b22c11fe1cf780
YWJamesLin <ywjameslin@gmail.com>
2021-09-26 18:08:08 +0800

    [Temperature] Auto detect coretemp monitor to prevent loading wrong monitor

--- awesome/temperature.lua
+++ awesome/temperature.lua
@@ -7,25 +7,32 @@ function temperatureInfo ()
   local temperatue = nil
   local result = ''
   local resultAvailable = false
+  local coreTempName = 'coretemp'
 
   for monCounter=0,16,1 do
-    for counter=1,16,1 do
-      fileHandler = io.open (path .. monCounter .. '/temp' .. counter .. '_input', "r")
-      if fileHandler ~= nil then
-        temperature = fileHandler:read() / 1000 .. '°C'
-        fileHandler:close()
-        if result == '' then
-          result = result .. temperature
-        else
-          result = result .. ' ' .. temperature
+    nameHandler = io.open (path .. monCounter .. '/name', "r")
+    if nameHandler ~= nil then
+      name = nameHandler:read()
+      nameHandler:close()
+      if name == coreTempName then
+        for counter=1,16,1 do
+          fileHandler = io.open (path .. monCounter .. '/temp' .. counter .. '_input', "r")
+          if fileHandler ~= nil then
+            temperature = fileHandler:read() / 1000 .. '°C'
+            fileHandler:close()
+            if result == '' then
+              result = result .. temperature
+            else
+              result = result .. ' ' .. temperature
+            end
+            resultAvailable = true
+          end
         end
-
-        resultAvailable = true
+        if resultAvailable then
+          result = result .. ' '
+          return result
+        end
       end
-    end
-    if resultAvailable then
-      result = result .. ' '
-      return result
     end
   end
 end
